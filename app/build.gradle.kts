9956d0a66486ab73d046a5919a83524d3bbe950c
EliasMena <eliasmenapech@gmail.com>
2023-10-22 16:38:15 -0600

    add ViewBinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,12 +30,17 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
 dependencies {
+
+
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
