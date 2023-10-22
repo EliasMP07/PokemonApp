e9e4ab97a2d6e03ac90d8c9e4486ddc0722e63a1
EliasMena <eliasmenapech@gmail.com>
2023-10-22 16:36:53 -0600

    init commit

---
+++ app/src/main/java/com/devdroid/pokemonapp/ui/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.devdroid.pokemonapp.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devdroid.pokemonapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
