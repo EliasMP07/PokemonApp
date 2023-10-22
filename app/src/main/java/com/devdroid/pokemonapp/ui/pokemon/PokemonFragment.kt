7bd8e580a38ca0ae7eb1cec90360dd374bcd3b71
EliasMena <eliasmenapech@gmail.com>
2023-10-22 16:58:35 -0600

    init add fragment for navigationComponent

---
+++ app/src/main/java/com/devdroid/pokemonapp/ui/pokemon/PokemonFragment.kt
@@ -0,0 +1,22 @@
+package com.devdroid.pokemonapp.ui.pokemon
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.devdroid.pokemonapp.R
+
+
+class PokemonFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pokemon, container, false)
+    }
+
+
+}
